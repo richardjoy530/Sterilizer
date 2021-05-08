c346651a7864be25508325cfefb2060d2c310ddc
Richard Joy <richardjoy530@gmail.com>
2021-05-09 00:26:38 +0530

    Add-device control flow UI done

--- lib/utils/firebase_manager.dart
+++ lib/utils/firebase_manager.dart
@@ -4,25 +4,21 @@ import 'package:firebase_database/firebase_database.dart';
 String localStates = "";
 
 class FirebaseManager {
-  DatabaseReference db;
+  static DatabaseReference db = FirebaseDatabase.instance.reference();
 
-  FirebaseManager() {
-    db = FirebaseDatabase.instance.reference();
-  }
-
-  add(Device device) {
+  static add(Device device) {
     db.child(device.id.toString()).child("motionDetected").set(1);
     db.child(device.id.toString()).child("uv").set("OFF");
   }
 
-  switchUV(Device device) async {
+  static switchUV(Device device) async {
     if (device.uv)
       db.child(device.id.toString()).child("uv").set("ON");
     else
       db.child(device.id.toString()).child("uv").set("OFF");
   }
 
-  sync(Device device) {
+  static sync(Device device) {
     db
         .child(device.id.toString())
         .child("uv")
@@ -31,7 +27,7 @@ class FirebaseManager {
     return device;
   }
 
-  updateSchedules(Device device) {
+  static updateSchedules(Device device) {
     List<Map<String, dynamic>> mapList = [];
     device.schedules.forEach((element) {
       var start =
@@ -52,20 +48,27 @@ class FirebaseManager {
         .set(mapList.length);
   }
 
-  void motionReset(Device device) {
+  static void motionReset(Device device) {
     db.child(device.id.toString()).child("motionDetected").set(1);
   }
 
-  String encodeScheduleDays(ScheduleData scheduleData) {
+  static String encodeScheduleDays(ScheduleData scheduleData) {
     String encodedValue = "0000000";
     var dayBool = scheduleData.stringify().split(" ");
-    if (dayBool[4 + 1] == "true") encodedValue = encodedValue.replaceRange(0, 1, '1');
-    if (dayBool[4 + 2] == "true") encodedValue = encodedValue.replaceRange(1, 2, '1');
-    if (dayBool[4 + 3] == "true") encodedValue = encodedValue.replaceRange(2, 3, '1');
-    if (dayBool[4 + 4] == "true") encodedValue = encodedValue.replaceRange(3, 4, '1');
-    if (dayBool[4 + 5] == "true") encodedValue = encodedValue.replaceRange(4, 5, '1');
-    if (dayBool[4 + 6] == "true") encodedValue = encodedValue.replaceRange(5, 6, '1');
-    if (dayBool[4 + 7] == "true") encodedValue = encodedValue.replaceRange(6, 7, '1');
+    if (dayBool[4 + 1] == "true")
+      encodedValue = encodedValue.replaceRange(0, 1, '1');
+    if (dayBool[4 + 2] == "true")
+      encodedValue = encodedValue.replaceRange(1, 2, '1');
+    if (dayBool[4 + 3] == "true")
+      encodedValue = encodedValue.replaceRange(2, 3, '1');
+    if (dayBool[4 + 4] == "true")
+      encodedValue = encodedValue.replaceRange(3, 4, '1');
+    if (dayBool[4 + 5] == "true")
+      encodedValue = encodedValue.replaceRange(4, 5, '1');
+    if (dayBool[4 + 6] == "true")
+      encodedValue = encodedValue.replaceRange(5, 6, '1');
+    if (dayBool[4 + 7] == "true")
+      encodedValue = encodedValue.replaceRange(6, 7, '1');
     return encodedValue;
   }
 }
